efdccb2d678918f4ee3b1b8fb396acb63f9bbeb2
Mark deVilliers <markdevilliers@gmail.com>
2014-07-23 22:50:33 +0100

    started making the tests pass with the DETS imple

--- src/ring_buffer_store.erl
+++ src/ring_buffer_store.erl
@@ -1,18 +1,29 @@
 -module (ring_buffer_store).
+-include_lib("stdlib/include/ms_transform.hrl").
 
 -export ([new/2,get/2,delete/1,insert/2]).
 
-% ets implementation
 new(ets, Name) ->
   TableId = ets:new(Name, [ordered_set, named_table]),
-  {?MODULE, ets, TableId}.
+  {?MODULE, ets, TableId};
+new(dets, Name) ->
+  {ok,TableId} = dets:open_file(Name, []),
+  {?MODULE, dets, TableId}.
 
 get(Position, {_, ets, TableId }) ->
   [{_, Value }] = ets:slot(TableId, Position),
+  Value;
+get(Position, {_, dets, TableId }) ->
+  [Value] = dets:select(TableId, ets:fun2ms(fun({Pos,Value}) when  Pos =:= Position -> Value end)),
   Value.
 
 delete({_, ets, TableId }) ->
-  true = ets:delete(TableId).
+  true = ets:delete(TableId);
+delete({_, dets, TableId }) ->
+  ok = dets:delete_all_objects(TableId),
+  file:delete(TableId).
 
 insert(Value,{_, ets, TableId }) ->
-  ets:insert(TableId, Value).
+  ets:insert(TableId, Value);
+insert(Value,{_, dets, TableId }) ->
+  dets:insert(TableId, Value).
--- test/basic_usage_tests.erl
+++ test/basic_usage_tests.erl
@@ -4,10 +4,13 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-basic_test() ->
+basic_ets_test() -> do_basic(ets).
+basic_dets_test() -> do_basic(dets).
+
+do_basic(Impl) ->
 	ring_buffer_sup:start_link(),
 	Entries = 5,
-	{ok, Ref} = ring_buffer:new(basic_test, Entries),
+	{ok, Ref} = ring_buffer:new(basic_test, Entries, Impl),
 	ok = ring_buffer:add(Ref, 1 ),
 	[1] = ring_buffer:select(Ref, 1),
 	1 = ring_buffer:count(Ref),
@@ -53,35 +56,49 @@ basic_test() ->
 	[11,10,9,8] = ring_buffer:select(Ref, 4),
 	[11,10,9] = ring_buffer:select(Ref, 3),
 	[11,10] = ring_buffer:select(Ref, 2),
-	[11] = ring_buffer:select(Ref, 1).
-	
-
-stop_and_clean_up_test() -> 
-	ring_buffer_sup:start_link(),
-	Entries = 5,
-	{ok, Ref} = ring_buffer:new(stop_and_clean_up_test, Entries),
+	[11] = ring_buffer:select(Ref, 1),
 	ring_buffer:delete(Ref).
 
-invalid_size_test() ->
+ets_stop_and_clean_up_test() -> do_stop_and_clean_up(ets).
+dets_stop_and_clean_up_test() -> do_stop_and_clean_up(dets).
+	
+do_stop_and_clean_up(Impl) -> 
 	ring_buffer_sup:start_link(),
 	Entries = 5,
-	{ok, Ref} = ring_buffer:new(invalid_count_test, Entries),
+	{ok, Ref} = ring_buffer:new(stop_and_clean_up_test, Entries, Impl),
+	ring_buffer:delete(Ref).
+
+ets_invalid_size_test() -> do_invalid_size(ets). 
+dets_invalid_size_test()-> do_invalid_size(dets).
+
+do_invalid_size(Impl) ->
+	ring_buffer_sup:start_link(),
+	Entries = 5,
+	{ok, Ref} = ring_buffer:new(invalid_count_test, Entries, Impl),
 	{error, invalid_length} = ring_buffer:select(Ref, 1), % nothing written yet
 	{error, invalid_length} = ring_buffer:select(Ref, 2), % nothing written yet
 	{error, invalid_length} = ring_buffer:select(Ref, 3), % nothing written yet
 	{error, invalid_length} = ring_buffer:select(Ref, 4), % nothing written yet
 	{error, invalid_length} = ring_buffer:select(Ref, 5), % nothing written yet
 	{error, invalid_length} = ring_buffer:select(Ref, -10), % negative number
-	{error, invalid_length} = ring_buffer:select(Ref, 99). % bigger than initial size 
+	{error, invalid_length} = ring_buffer:select(Ref, 99), % bigger than initial size 
+	ring_buffer:delete(Ref).
 
-get_default_size_test() ->
-	ring_buffer_sup:start_link(),
-	{ok, Ref} = ring_buffer:new(get_default_size_test),
-	512 = ring_buffer:size(Ref).
+ets_get_default_size_test() -> do_get_default_size(ets). 
+dets_get_default_size_test()-> do_get_default_size(dets).
 
-reset_state_test() ->
+do_get_default_size(Impl) ->
 	ring_buffer_sup:start_link(),
-	{ok, Ref} = ring_buffer:new(reset_state_test),
+	{ok, Ref} = ring_buffer:new(get_default_size_test, Impl),
+	512 = ring_buffer:size(Ref),
+	ring_buffer:delete(Ref).
+
+ets_reset_state_test() -> do_reset_state_test(ets). 
+dets_reset_state_test()-> do_reset_state_test(dets).
+
+do_reset_state_test(Impl) ->
+	ring_buffer_sup:start_link(),
+	{ok, Ref} = ring_buffer:new(reset_state_test, Impl),
 	ok = ring_buffer:add(Ref, 1 ),
 	ok = ring_buffer:add(Ref, 1 ),
 	ok = ring_buffer:add(Ref, 1 ),
@@ -93,28 +110,27 @@ reset_state_test() ->
 	ok = ring_buffer:add(Ref, 1 ),
 	1 = ring_buffer:count(Ref),
 	ok =  ring_buffer:clear(Ref),
-	0 = ring_buffer:count(Ref).
+	0 = ring_buffer:count(Ref),
+	ring_buffer:delete(Ref).
 
+ets_starting_multiple_ring_buffers_of_the_same_name_test() -> do_starting_multiple_ring_buffers_of_the_same_name(ets). 
+% dets_starting_multiple_ring_buffers_of_the_same_name_test()-> do_starting_multiple_ring_buffers_of_the_same_name(dets).
 
-starting_multiple_ring_buffers_of_the_same_name_test()->
+do_starting_multiple_ring_buffers_of_the_same_name(Impl)->
 	ring_buffer_sup:start_link(),
-	{ok, Ref} = ring_buffer:new(starting_multiple_ring_buffers_of_the_same_name_test, 5),
-	{ok, Ref} = ring_buffer:new(starting_multiple_ring_buffers_of_the_same_name_test, 5),
-	{ok, Ref} = ring_buffer:new(starting_multiple_ring_buffers_of_the_same_name_test, 5),
-	{ok, Ref} = ring_buffer:new(starting_multiple_ring_buffers_of_the_same_name_test, 5),
-	{ok, Ref} = ring_buffer:new(starting_multiple_ring_buffers_of_the_same_name_test, 5),
+	{ok, Ref} = ring_buffer:new(starting_multiple_ring_buffers_of_the_same_name_test, 5, Impl),
+	{ok, Ref} = ring_buffer:new(starting_multiple_ring_buffers_of_the_same_name_test, 5, Impl),
+	{ok, Ref} = ring_buffer:new(starting_multiple_ring_buffers_of_the_same_name_test, 5, Impl),
+	{ok, Ref} = ring_buffer:new(starting_multiple_ring_buffers_of_the_same_name_test, 5, Impl),
+	{ok, Ref} = ring_buffer:new(starting_multiple_ring_buffers_of_the_same_name_test, 5, Impl),
 	{ok, Ref} = ring_buffer:find(starting_multiple_ring_buffers_of_the_same_name_test),
- 	ok.
+    ring_buffer:delete(Ref).
 
 exists_on_non_existent_buffer_test() ->
 	undefined = ring_buffer:find(does_not_exist).
 
-
-
 large_test_test() ->
-    {timeout, 30, fun() -> 
-                 large_buffer()
-          end }.
+    {timeout, 30, fun() -> large_buffer() end }.
 
 large_buffer() ->
 	ring_buffer_sup:start_link(),
